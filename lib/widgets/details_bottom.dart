ee9af8408be71ed28e9157b867ad8930e80e35b5
Mert Can YILMAZ <mertcanylmaz@gmail.com>
2022-03-24 17:19:10 +0300

    UI Changes

--- lib/widgets/details_bottom.dart
+++ lib/widgets/details_bottom.dart
@@ -11,68 +11,64 @@ class DetailsBottom extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 30, right: 30, bottom: 30),
-      child: Expanded(
-        child: SingleChildScrollView(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              RichText(
-                text: TextSpan(
-                  text: "\$410",
-                  style: GoogleFonts.varelaRound(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          RichText(
+            text: TextSpan(
+              text: "\$410",
+              style: GoogleFonts.varelaRound(
+                  color: Utils.propertyNameColor,
+                  fontSize: 30,
+                  height: 1.4,
+                  fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                  text: "/Person",
+                  style: TextStyle(
                       color: Utils.propertyNameColor,
-                      fontSize: 30,
-                      height: 1.4,
-                      fontWeight: FontWeight.bold),
-                  children: [
-                    TextSpan(
-                      text: "/Person",
-                      style: TextStyle(
-                          color: Utils.propertyNameColor,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600),
                 ),
-              ),
-              Spacer(),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.4,
-                height: 50,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(13),
-                    color: Utils.mainOrange,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Utils.mainOrange.withOpacity(0.2),
-                          blurRadius: 10,
-                          spreadRadius: 3,
-                          offset: Offset(0.0, 0))
-                    ]),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Continue",
-                        style: GoogleFonts.varelaRound(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w800),
-                      ),
-                      SizedBox(width: 5),
-                      Icon(
-                        Icons.arrow_forward_rounded,
-                        color: Colors.white,
-                        size: 20,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+          Spacer(),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.4,
+            height: 50,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(13),
+                color: Utils.mainOrange,
+                boxShadow: [
+                  BoxShadow(
+                      color: Utils.mainOrange.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                      offset: Offset(0.0, 0))
+                ]),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Continue",
+                    style: GoogleFonts.varelaRound(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  SizedBox(width: 5),
+                  Icon(
+                    Icons.arrow_forward_rounded,
+                    color: Colors.white,
+                    size: 20,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
